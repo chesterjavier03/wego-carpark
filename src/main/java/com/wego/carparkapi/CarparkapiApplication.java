0e0e8476335a6d1101271f9e97a56e32d5b52a4f
Chester Javier <chesterjavier03@duck.com>
2025-07-23 15:22:01 +0800

    Wego Coding Exercise - Booking API: - Initial Commit

---
+++ src/main/java/com/wego/carparkapi/CarparkapiApplication.java
@@ -0,0 +1,13 @@
+package com.wego.carparkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarparkapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarparkapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wego/carparkapi/CarparkapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wego.carparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparkapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
